900ee54dcd493c65b7cedc53d41842ac8fd7de6f
Alecprice <52548136+Alecprice@users.noreply.github.com>
2022-06-14 23:49:12 -0400

    Add files via upload

---
+++ Project 1/CSC470_Prj1_Pt1/CSC470_Prj1_Pt1/CalculatorOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSC470_Prj1_Pt1
+{
+	class Calculator
+	{
+		public static double DoOperation(double num1, double num2, string mathOperation)
+		{
+			double result = double.NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error.
+
+
+			switch (mathOperation)
+			{
+				case "a":
+				case "add":
+				case "+":
+					result = num1 + num2;
+					break;
+				case "s":
+				case "subtract":
+				case "-":
+					result = num1 - num2;
+					break;
+				case "m":
+				case "multiply":
+				case "*":
+					result = num1 * num2;
+					break;
+				case "d":
+				case "divide":
+				case "/":
+					if (num2 != 0)
+					{
+						result = num1 / num2;
+					}
+
+					break;
+
+				// Return text for an incorrect entry.
+				default:
+					break;
+			}
+			return result;
+		}
+	}
+}
